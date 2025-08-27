e1f5ddb8b16819780821dd1866025e309a86973f
Anupam Yadav (Anurag) <anuragyadavanupam@gmail.com>
2025-08-27 15:34:08 +0530

    Add files via upload

---
+++ DSA/Zigzag Pattern.java
@@ -0,0 +1,15 @@
+public class ZigzagPattern {
+    public static void main(String[] args) {
+        int rows = 7;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < rows; j++) {
+                if (i == j || j == rows - i - 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
