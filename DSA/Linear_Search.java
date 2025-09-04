8bb660fa54ed10ee76a0d6e6b81db02d8917960c
Anupam Yadav (Anurag) <anuragyadavanupam@gmail.com>
2025-09-04 11:58:33 +0530

    Create Linear_Search.java

---
+++ DSA/Linear_Search.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class Linear_Search
+  {
+    piblic static void main(string args[])
+    {
+          sacnner sc = new Scanner(String args[])
+          int a[]=new int[5];
+           int flag=0;
+            for(int i=0; i<5;i++)
+              {   
+                  System.out.println("Enter Number: ");
+                   a[i]=sc.nextInt();
+              }
+              System.out.print("enter No.")
+                }
+  }
