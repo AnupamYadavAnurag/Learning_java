11d4986eb31a39852f7f68cf8817119f91a68a94
Anupam Yadav (Anurag) <anuragyadavanupam@gmail.com>
2025-09-07 21:45:44 +0530

    Create OverloadingStudent.java

---
+++ OverloadingStudent.java
@@ -0,0 +1,30 @@
+class Student {
+    String name;
+    int roll;
+
+    // No-arg constructor
+    Student() {
+        name = "Unknown";
+        roll = 0;
+    }
+
+    // Parameterized constructor
+    Student(String n, int r) {
+        name = n;
+        roll = r;
+    }
+
+    void display() {
+        System.out.println("Name: " + name + ", Roll: " + roll);
+    }
+}
+
+public class ConstructorOverloadDemo {
+    public static void main(String[] args) {
+        Student s1 = new Student();                // calls no-arg
+        Student s2 = new Student("Anupam", 101);   // calls param constructor
+
+        s1.display();
+        s2.display();
+    }
+}
