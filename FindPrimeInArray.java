e64198be1c05a62c8d48d8539ea94a1be2c0e191
Anupam Yadav (Anurag) <anuragyadavanupam@gmail.com>
2025-10-09 14:01:50 +0530

    Create FindPrimeInArray.java

---
+++ FindPrimeInArray.java
@@ -0,0 +1,21 @@
+public class SimplePrimeArray {
+    public static void main(String[] args) {
+        int[] arr = {5, 7, 9, 11, 15};
+
+        System.out.println("Prime numbers in the array:");
+
+        for (int n : arr) {
+            int count = 0;
+
+            for (int i = 1; i <= n; i++) {
+                if (n % i == 0) {
+                    count++;
+                }
+            }
+
+            if (count == 2) { // only divisible by 1 and itself
+                System.out.println(n);
+            }
+        }
+    }
+}
