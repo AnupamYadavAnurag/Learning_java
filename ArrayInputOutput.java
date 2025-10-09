854c1299a6d07aff911d87cddb0f1b50eae59182
Anupam Yadav (Anurag) <anuragyadavanupam@gmail.com>
2025-10-09 14:41:45 +0530

    Update ArrayInputOutput.java

--- ArrayInputOutput.java
+++ ArrayInputOutput.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class TwoDArrayExample {
+public class ArrayInputOutput {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
