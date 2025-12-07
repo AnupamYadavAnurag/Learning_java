0121ae4ad0f39a19214b69712d0872a1d145faae
Anupam Yadav (Anurag) <anuragyadavanupam@gmail.com>
2025-12-07 10:56:58 +0530

    Rename PaymentTest to PaymentTest2

---
+++ OOPS/PaymentTest2.java
@@ -0,0 +1,41 @@
+class Payment {
+    void pay() {
+        System.out.println("Payment in general.");
+    }
+}
+
+class UPI extends Payment {
+    @Override
+    void pay() {
+        System.out.println("Payment through UPI (QR scan).");
+    }
+}
+
+class Card extends Payment {
+    @Override
+    void pay() {
+        System.out.println("Payment through Debit/Credit card.");
+    }
+}
+
+class Wallet extends Payment {
+    @Override
+    void pay() {
+        System.out.println("Payment through Wallet balance.");
+    }
+}
+
+public class PaymentTest2 {
+    public static void main(String[] args) {
+        Payment p;
+
+        p = new UPI();
+        p.pay();
+
+        p = new Card();
+        p.pay();
+
+        p = new Wallet();
+        p.pay();
+    }
+}
